e375582684f2f4cb9e31e3e300f8828d3e3c3359
mallikarjun25 <68541427+mallikarjun25@users.noreply.github.com>
2020-07-20 17:17:46 +0530

    Create html.java

---
+++ html.java
@@ -0,0 +1,101 @@
+body
+{
+margin: 0;
+padding: 20px;
+}
+.education-icon{
+    width: 40px;
+    height: 40px;
+    object-fit: contain;
+}
+
+.sign-in-form
+{
+width: 350px;
+box-shadow: 0 0 3px 0 rgba(37, 37, 37, 0.74);
+background: #fff;
+padding: 40px;
+margin: 8% auto 0;
+text-align: center;
+}
+
+
+.input-box
+{
+font-family:"Roboto",sans-serif;
+outline: 1;
+background: #f2f2f2;
+width: 108.5%;
+border: 0;
+padding: 15px;
+margin: 10px 150px 3px -3px;
+box-sizing: border-box;
+font-size: 14px;
+text-align: left;
+border-radius:4px ;
+}
+
+
+
+
+button 
+{
+
+font-family:"Roboto",sans-serif;
+text-transform: uppercase;
+
+background: #53D1EA;
+width: 96%;
+height: 45px;
+border: 0;
+padding: 0%;
+font-weight: bold;
+color: #FFFFFF;
+font-size: 18pt;
+border-radius:12px ;
+}
+.button1
+{
+background-color:#3B5998;
+border-radius:12px ;
+text-transform: lowercase;
+text-align: center;
+width: 100%;
+}
+
+.button2
+{
+background-color: #dd4b39;
+border-radius:12px ;
+text-transform: lowercase;
+text-align: center;
+width: 100%;
+}
+.button3
+{
+background-color: #D0DB00;
+text-transform: italic;
+border-radius:12px ;
+width: 90%;
+height: 50px;
+}
+h2
+{
+
+    background-color: #53D1EA;
+    
+}
+
+
+h3 {
+    width: 100%; 
+    text-align: center; 
+    border-bottom: 1px solid #000; 
+    line-height: 0.1em;
+    margin: 10px 0 20px; 
+ } 
+ 
+ h3 span { 
+     background:#fff; 
+     padding:0 10px; 
+ }
